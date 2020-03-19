e0633fe4be2dff5be9b7bc619ae991cbf34ee272
Gyakusetsu <reymarbc@gmail.com>
2020-03-19 10:55:55 +0800

    Changed icon and covid url

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -61,7 +61,7 @@ export default {
     },
     redirectToCovid() {
       // window.location.href = "https://drive.google.com/open?id=1a7BAAsDKU-iI57K4MlUJ5r6JchFP7Xxt"
-      this.redirectMe("http://gyakusetsu.github.io/COVID-19-Watcher");
+      this.redirectMe("http://gyakusetsu.github.io/coronavirus-map");
     },
     redirectMe(url) {
       window.open(url);
