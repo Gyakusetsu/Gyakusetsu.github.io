288acab35f5033eebd054436c3ea0f2b8802a824
Reymar Clemente (IS-PH) <reymar_clemente@trendmicro.com>
2020-08-05 00:21:06 +0800

    Added some fixes

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,11 @@ set -e
 # build
 mkdir dist
 
-mv favicon.ico dist
-mv index.html dist
-mv CNAME dist
+cp favicon.ico dist
+cp index.html dist
+cp life.png dist
+cp styles.css dist
+cp CNAME dist
 
 # navigate into the build output directory
 cd dist
@@ -18,6 +20,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/Gyakusetsu/gyakusetsu.github.io.git master
+git push -f git@github.com:Gyakusetsu/gyakusetsu.github.io.git master
 
 cd -
+
